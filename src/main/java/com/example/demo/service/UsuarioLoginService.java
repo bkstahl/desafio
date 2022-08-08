fdb412c3a5e5e19da87d594b6084ccf1ce597553
Bruno Konzen Stahl <bkstahl@gmail.com>
2022-08-08 18:04:54 -0300

    Update UsuarioLoginService.java
    Alterando tipo de anotação

--- src/main/java/com/example/demo/service/UsuarioLoginService.java
+++ src/main/java/com/example/demo/service/UsuarioLoginService.java
@@ -4,12 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Usuario;
 import com.example.demo.repository.UsuarioRepository;
 
-@Repository
+@Service
 public class UsuarioLoginService implements UserDetailsService{
 
 	@Autowired
